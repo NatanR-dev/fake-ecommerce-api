82fdbe8af145564685dee1e1a31ebc0ad49a2251
Natan Ribeiro <natan.ribeiro@precode.com.br>
2023-07-20 17:26:44 -0300

    feat: added a json products to the api and customized the welcomepage

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const items = require("./data.json");
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send(`Hello World<br/> The port <b>${port}</b> is running!`);
+  res.send(
+    `<div style="text-align: center">
+        <strong>Bem-vindo à API fake-ecommerce!</strong><br/>
+        Acesse a rota <a href="/api/items">/api/items</a> para obter os produtos.
+     </div>`
+  );
+});
+
+app.get("/api/items", (req, res) => {
+  res.json(items);
 });
 
 const port = 3000;
